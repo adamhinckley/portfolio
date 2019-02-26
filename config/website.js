2965089cf9d25e1e9a35a591e0d232d06f1da102
adamhinckley <adamhinckley@mac.com>
2019-02-26 16:04:23 -0700

    replaces favicons again. . .this theme seems hacked

--- config/website.js
+++ config/website.js
@@ -3,14 +3,15 @@ const tailwind = require("../tailwind")
 module.exports = {
     pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-    siteTitle: "Adam H", // Navigation and Site Title
-    siteTitleAlt: "Adam", // Alternative Site title for SEO
+    siteTitle: "Adam Hinckley", // Navigation and Site Title
+    siteTitleAlt: "Adam Hinckley", // Alternative Site title for SEO
     siteTitleShort: "Adam", // short_name for manifest
     siteHeadline: "Creating marvelous art & blazginly fast websites", // Headline for schema.org JSONLD
     siteUrl: "https://adamhinckley.com", // Domain of your site. No trailing slash!
     siteLanguage: "en", // Language Tag on <html> element
     siteLogo: "/logo.png", // Used for SEO and manifest
-    siteDescription: "Playful & Colorful One-Page website with Parallax effect",
+    siteDescription:
+        "Adam is a a former life insurance agency owner that found a love for web development after creating underwriting solutions for the agents he worked with.",
     author: "Adam Hinckley", // Author for schema.org JSONLD
 
     // siteFBAppID: '123456789', // Facebook App ID - Optional
