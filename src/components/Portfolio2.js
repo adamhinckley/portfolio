4e6a2a8264e37ee1f7741b79726451e5881d4e98
adamhinckley <adamhinckley@mac.com>
2019-03-13 14:48:03 -0600

    padding for better readability on medication

--- src/components/Portfolio2.js
+++ src/components/Portfolio2.js
@@ -23,15 +23,26 @@ import Logo from "../assets/images/logo2.png"
 const Portfolio = () => (
     <section id="portfolio">
         <div className="row">
-            <div className="twelve columns collapsed">
+            <div className="project-title">
                 <h1>Check Out Some of My Projects</h1>
             </div>
             <div className="projects-container">
                 <div className="project">
                     <h1>Cadence</h1>
+                    <p className="project-tagline">SaaS Employee Scheduling</p>
+                    <div className="project-links">
+                        <a href="https://getcadence.co/">Visit Site</a>
+                        <a href="https://github.com/Lambda-School-Labs/labs9-employee-scheduler">GitHub</a>
+                    </div>
                 </div>
-
-                <div className="project" />
+                <div className="project">
+                    <h1>Risk Assessment</h1>
+                    <p className="project-tagline">PWA Mobile underwriting tool</p>
+                    <div className="project-links">
+                        <a href="https://risk-assesment.netlify.com/">Visit Site</a>
+                        <a href="https://github.com/adamhinckley/Insurance-Risk-Assesment">GitHub</a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
